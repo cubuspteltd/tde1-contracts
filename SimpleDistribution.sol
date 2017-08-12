9aa82fefd1d0944e6cfdc6ed5b749833c0a45e93
Pavel Isayenko <pavel.isayenko@gmail.com>
2017-08-12 22:37:53 +0300

    Initial commit

---
+++ SimpleDistribution.sol
@@ -0,0 +1,139 @@
+pragma solidity ^0.4.11;
+
+import "./SafeMathLib.sol";
+import "./SimpleToken.sol";
+
+/*
+ * Haltable
+ *
+ * Abstract contract that allows children to implement an
+ * emergency stop mechanism. Differs from Pausable by causing a throw when in halt mode.
+ *
+ *
+ * Originally envisioned in FirstBlood ICO contract.
+ */
+contract Haltable is Ownable {
+  bool public halted;
+
+  modifier stopInEmergency {
+    if (halted) revert();
+    _;
+  }
+
+  modifier onlyInEmergency {
+    if (!halted) revert();
+    _;
+  }
+
+  // called by the owner on emergency, triggers stopped state
+  function halt() external onlyOwner {
+    halted = true;
+  }
+
+  // called by the owner on end of emergency, returns to normal state
+  function unhalt() external onlyOwner onlyInEmergency {
+    halted = false;
+  }
+
+}
+
+
+
+contract SimpleDistribution is Haltable {
+
+  using SafeMathLib for uint;
+
+  address public wallet;
+  uint public start;
+  uint public end;
+  SimpleToken public token;
+  uint public weiGoal;
+  uint public contributorsCount = 0;
+  uint public weiTotal = 0;
+  uint public tokensSold = 0;
+  uint public loadedRefund = 0;
+  uint public weiRefunded = 0;
+  mapping (address => uint) public contributed;
+
+  enum States {Preparing, Distribution, Success, Failure, Refunding}
+
+  event Contributed(address contributor, uint weiAmount, uint tokenAmount);
+  event Refund(address contributor, uint weiAmount);
+
+  function SimpleDistribution(SimpleToken _token, address _wallet, uint _start, uint _end, uint _weiGoal) {
+    
+    if (_token == address(0) || _wallet == address(0) || _start == 0 || _end == 0 || _start >= _end) revert();
+
+    token = _token;
+    
+    if (!token.isToken()) revert();
+    
+    start = _start;
+    end = _end;
+    weiGoal = _weiGoal;
+    wallet = _wallet;
+
+    token.initialize(this, wallet);
+    if (token.distributionContract() != address(this)) revert();
+  }
+
+  function contributeInternal(address receiver, uint weiAmount) stopInEmergency internal {
+    uint tokenAmount = weiAmount * 1000;
+    if (contributed[receiver] == 0) contributorsCount++;
+    contributed[receiver] = contributed[receiver].plus(weiAmount);    
+    tokensSold = tokensSold.plus(tokenAmount);
+    weiTotal = weiTotal.plus(weiAmount);
+    token.distribute(wallet, receiver, tokenAmount);
+    wallet.transfer(weiAmount);
+    Contributed(receiver, weiAmount, tokenAmount);
+  }
+
+  /*
+  * Contributors can make payment and receive their tokens
+  */
+  function contribute() payable inState(States.Distribution) {
+    if (msg.value == 0) revert();
+    contributeInternal(msg.sender, msg.value);
+  }
+
+  /*
+   * Allow load refunds back on the contract for the refunding.
+   */
+  function loadRefund() payable inState(States.Failure) {
+    if(msg.value == 0) revert();
+    loadedRefund = loadedRefund.plus(msg.value);
+  }
+
+  /*
+   * Contributors can claim refund.
+   */
+  function refund() inState(States.Refunding) {
+    uint weiValue = contributed[msg.sender];
+    if (weiValue == 0) revert();
+    contributed[msg.sender] = 0;
+    weiRefunded = weiRefunded.plus(weiValue);
+    Refund(msg.sender, weiValue);
+    msg.sender.transfer(weiValue);
+  }
+
+  function() payable {
+    revert();
+  }
+
+  /*
+  * State machine
+  */
+  function getState() constant returns (States) {
+    if (token.distributionContract() == address(0) || now < start) return States.Preparing;
+    else if (now >= start && now < end) return States.Distribution;
+    else if (weiTotal >= weiGoal) return States.Success;
+    else if (now >= end && weiTotal < weiGoal && loadedRefund == 0) return States.Failure;
+    else if (loadedRefund > 0) return States.Refunding;
+  }
+
+  modifier inState(States state) {
+    if(getState() != state) revert();
+    _;
+  }
+
+}
---
+++ SimpleToken.sol
@@ -0,0 +1,411 @@
+pragma solidity ^0.4.11;
+
+import "./SafeMathLib.sol";
+
+/*
+ * ERC20 interface
+ * see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 {
+  uint public totalSupply;
+  function balanceOf(address who) constant returns (uint);
+  function allowance(address owner, address spender) constant returns (uint);
+
+  function transfer(address to, uint value) returns (bool ok);
+  function transferFrom(address from, address to, uint value) returns (bool ok);
+  function approve(address spender, uint value) returns (bool ok);
+  event Transfer(address indexed from, address indexed to, uint value);
+  event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+
+
+/**
+ * Math operations with safety checks
+ */
+contract SafeMath {
+  function safeMul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function safeDiv(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+  function safeSub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function safeAdd(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
+
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      revert();
+    }
+  }
+}
+
+
+
+/**
+ * Standard ERC20 token with Short Hand Attack and approve() race condition mitigation.
+ *
+ * Based on code by FirstBlood:
+ * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract StandardToken is ERC20, SafeMath {
+
+  /* A new owner received tokens */
+  event Sold(address receiver, uint amount);
+
+  /* Actual balances of token holders */
+  mapping(address => uint) balances;
+
+  /* approve() allowances */
+  mapping (address => mapping (address => uint)) allowed;
+
+  /* Interface declaration */
+  function isToken() public constant returns (bool weAre) {
+    return true;
+  }
+
+  /**
+   *
+   * Fix for the ERC20 short address attack
+   *
+   * http://vessenes.com/the-erc20-short-address-attack-explained/
+   */
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length < size + 4) {
+       revert();
+     }
+     _;
+  }
+
+  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) returns (bool success) {
+    balances[msg.sender] = safeSub(balances[msg.sender], _value);
+    balances[_to] = safeAdd(balances[_to], _value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+    uint _allowance = allowed[_from][msg.sender];
+
+    balances[_to] = safeAdd(balances[_to], _value);
+    balances[_from] = safeSub(balances[_from], _value);
+    allowed[_from][msg.sender] = safeSub(_allowance, _value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) constant returns (uint balance) {
+    return balances[_owner];
+  }
+
+  function approve(address _spender, uint _value) returns (bool success) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) revert();
+
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint remaining) {
+    return allowed[_owner][_spender];
+  }
+
+}
+
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    require(newOwner != address(0));      
+    owner = newOwner;
+  }
+
+}
+
+/**
+ * Upgrade agent interface inspired by Lunyr.
+ *
+ * Upgrade agent transfers tokens to a new contract.
+ * Upgrade agent itself can be the token contract, or just a middle man contract doing the heavy lifting.
+ */
+contract UpgradeAgent {
+
+  uint public originalSupply;
+
+  /** Interface marker */
+  function isUpgradeAgent() public constant returns (bool) {
+    return true;
+  }
+
+  function upgradeFrom(address _from, uint256 _value) public;
+
+}
+
+
+/**
+ * A token upgrade mechanism where users can opt-in amount of tokens to the next smart contract revision.
+ *
+ * First envisioned by Golem and Lunyr projects.
+ */
+contract UpgradeableToken is StandardToken {
+
+  /** Contract / person who can set the upgrade path. This can be the same as team multisig wallet, as what it is with its default value. */
+  address public upgradeMaster;
+
+  /** The next contract where the tokens will be migrated. */
+  UpgradeAgent public upgradeAgent;
+
+  /** How many tokens we have upgraded by now. */
+  uint256 public totalUpgraded;
+
+  /**
+   * Upgrade states.
+   *
+   * - NotAllowed: The child contract has not reached a condition where the upgrade can bgun
+   * - WaitingForAgent: Token allows upgrade, but we don't have a new agent yet
+   * - ReadyToUpgrade: The agent is set, but not a single token has been upgraded yet
+   * - Upgrading: Upgrade agent is set and the balance holders can upgrade their tokens
+   *
+   */
+  enum UpgradeState {Unknown, NotAllowed, WaitingForAgent, ReadyToUpgrade, Upgrading}
+
+  /**
+   * Somebody has upgraded some of his tokens.
+   */
+  event Upgrade(address indexed _from, address indexed _to, uint256 _value);
+
+  /**
+   * New upgrade agent available.
+   */
+  event UpgradeAgentSet(address agent);
+
+  /**
+   * Do not allow construction without upgrade master set.
+   */
+  function UpgradeableToken(address _upgradeMaster) {
+    upgradeMaster = _upgradeMaster;
+  }
+
+  /**
+   * Allow the token holder to upgrade some of their tokens to a new contract.
+   */
+  function upgrade(uint256 value) public {
+
+      UpgradeState state = getUpgradeState();
+      if(!(state == UpgradeState.ReadyToUpgrade || state == UpgradeState.Upgrading)) {
+        // Called in a bad state
+        revert();
+      }
+
+      // Validate input value.
+      if (value == 0) revert();
+
+      balances[msg.sender] = safeSub(balances[msg.sender], value);
+
+      // Take tokens out from circulation
+      totalSupply = safeSub(totalSupply, value);
+      totalUpgraded = safeAdd(totalUpgraded, value);
+
+      // Upgrade agent reissues the tokens
+      upgradeAgent.upgradeFrom(msg.sender, value);
+      Upgrade(msg.sender, upgradeAgent, value);
+  }
+
+  /**
+   * Set an upgrade agent that handles
+   */
+  function setUpgradeAgent(address agent) external {
+
+      if(!canUpgrade()) {
+        // The token is not yet in a state that we could think upgrading
+        revert();
+      }
+
+      if (agent == 0x0) revert();
+      // Only a master can designate the next agent
+      if (msg.sender != upgradeMaster) revert();
+      // Upgrade has already begun for an agent
+      if (getUpgradeState() == UpgradeState.Upgrading) revert();
+
+      upgradeAgent = UpgradeAgent(agent);
+
+      // Bad interface
+      if(!upgradeAgent.isUpgradeAgent()) revert();
+      // Make sure that token supplies match in source and target
+      if (upgradeAgent.originalSupply() != totalSupply) revert();
+
+      UpgradeAgentSet(upgradeAgent);
+  }
+
+  /**
+   * Get the state of the token upgrade.
+   */
+  function getUpgradeState() public constant returns(UpgradeState) {
+    if(!canUpgrade()) return UpgradeState.NotAllowed;
+    else if(address(upgradeAgent) == 0x00) return UpgradeState.WaitingForAgent;
+    else if(totalUpgraded == 0) return UpgradeState.ReadyToUpgrade;
+    else return UpgradeState.Upgrading;
+  }
+
+  /**
+   * Change the upgrade master.
+   *
+   * This allows us to set a new owner for the upgrade mechanism.
+   */
+  function setUpgradeMaster(address master) public {
+      if (master == 0x0) revert();
+      if (msg.sender != upgradeMaster) revert();
+      upgradeMaster = master;
+  }
+
+  /**
+   * Child contract can enable to provide the condition when the upgrade can begun.
+   */
+  function canUpgrade() public constant returns(bool) {
+     return true;
+  }
+
+}
+
+
+contract SimpleToken is UpgradeableToken, Ownable {
+
+  using SafeMathLib for uint;
+
+  string public name;
+  string public symbol;
+  uint public decimals;
+
+  address public distributionContract;
+
+  bool public released = false;
+
+  /*
+  * Only distribution contract allowed
+  */
+  modifier onlyDistribution() {
+    if (msg.sender != distributionContract) revert();
+    _;
+  }
+
+  /**
+   * Limit token transfer until the distribution is over.
+   *
+   */
+  modifier canTransfer(address _sender) {
+    if (!released) revert();
+    _;
+  }
+
+  function SimpleToken(string _name, string _symbol, uint _totalSupply, uint _decimals) UpgradeableToken(msg.sender) {
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
+    totalSupply = _totalSupply;
+  }
+
+  /**
+   * Owner can allow a distribution contract to assign tokens, all balance is set to wallet address.
+   */
+  function initialize(address _distributionContract, address _initialAddr) {
+    if (distributionContract != address(0)) revert();
+    if (_distributionContract == address(0) || _initialAddr == address(0)) revert();
+    distributionContract = _distributionContract;
+    balances[_initialAddr] = totalSupply;
+  }
+
+  /*
+  * This function sends tokens from wallet to contributors
+  * Only distribution contract can assign tokens
+  */
+  function distribute(address _sender, address _receiver, uint _amount) onlyDistribution {
+    balances[_receiver] = balances[_receiver].plus(_amount);
+    balances[_sender] = balances[_sender].minus(_amount);
+    Transfer(_sender, _receiver, _amount);
+  }
+
+  /**
+   * One way function to release the tokens to the wild.
+   */
+  function releaseTokenTransfer() onlyOwner {
+    released = true;
+  }
+
+  function transfer(address _to, uint _value) canTransfer(msg.sender) returns (bool success) {
+    // Call StandardToken.transfer()
+   return super.transfer(_to, _value);
+  }
+
+  function transferFrom(address _from, address _to, uint _value) canTransfer(_from) returns (bool success) {
+    // Call StandardToken.transferForm()
+    return super.transferFrom(_from, _to, _value);
+  }
+
+}
+
